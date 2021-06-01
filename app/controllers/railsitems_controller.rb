38273c6f416523316a15340583a201b5c05b1427
akshy <akshy.nattu@gmail.com>
2021-06-02 00:40:54 +0530

    Tues final

--- app/controllers/railsitems_controller.rb
+++ app/controllers/railsitems_controller.rb
@@ -1,7 +1,11 @@
 class RailsitemsController < ApplicationController
     def index
-        hashmap_or_array = [{ id: 1, name: "foo", email: "foo@example.org" },
-             { id: 2, name: "bar", email: "foo@example.org" }]
+        hashmap_or_array = [{ id: 1, name: "Beats Solo",
+            f1: "Switch seamlessly between Noise Cancelling with real-time audio calibration" ,
+            f2: "Upto 40 hrs of Battery back up with Noise cancellation off"},
+             { id: 2, name: "Nike shoe",
+                 f1: "Sole completely made of breathable material",
+                 f2: "In house engineered for ergonomic design and comfort" }]
         render json: hashmap_or_array
     end
 end
